519bf2619d3e11c092bc29c9394056a93d8165b0
maman4 <mamanadifirmansyah@gmail.com>
2023-11-07 05:30:52 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function navSlide() {
+    const burger = document.querySelector(".burger")
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active")
+        burger.classList.toggle("toggle-burger")
+    })
+}
+
+
+
+navSlide()
+
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+}
+
